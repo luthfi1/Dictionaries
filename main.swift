1ed30979c44b090f9bd30a0278b32551e742ae52
Muhammad Luthfi Farizqi Luthfi <luthfifarizqi23@gmail.com>
2020-10-21 01:34:05 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,64 @@
+//Dictionaries3
+let kumpulan  =  [
+  String:String
+]
+
+let kumpulan  = [
+  "Sekolah":"IDN"
+]
+
+let kumpulan = (sekolah:"IDN")
+
+
+
+
+
+
+/*
+//Dictionaries5
+var tim = [String:String]()
+tim ["bola"] = "Barcelona"
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//Dictionaries4
+var tim = [String:String]()
+*/
+
+/*
+//Dictionaries1
+let kumpulan = [
+  String:Double
+]
+
+let kumpulan = [
+  "Sekolah":2.10
+]
+let kumpulan = (sekolah:"sekolah")
+*/
+
+/*
+//Dictionaries2
+let kumpulan = [
+  Int:String
+]
+
+let kumpulan = [
+  "Sekolah":2020
+]
+let kumpulan = (sekolah:"sekolah")
+*/
+
